ab8a0236df38c23504bda43106f9296b9ce29c97
Tim Crombie <tim.crombie@gmail.com>
2024-02-29 18:15:34 -0500

    update lab7 proc

--- code/lab7_process_blast_results.R
+++ code/lab7_process_blast_results.R
@@ -3,29 +3,69 @@ library(tidyverse)
 # set the working directory to allow relative paths to project directory
 setwd(glue::glue("{dirname(rstudioapi::getActiveDocumentContext()$path)}/.."))
 
-# load blast results
-blast <- read_tsv("data/raw/rhab_blast.tsv2")
+# load blast results -YOU NEED TO HAVE THE EXACT PROJECT DIR STRUCTURE TO HAVE THIS COMMAND WORK!
+blast <- read_tsv("data/raw/rhab_blast_results.tsv")
 
+# let's glimpse the results in R
+glimpse(blast)
+
+# OK, now let's view the data in R, scroll around to see what you got.
+view(blast)
+
+#------------------------------------------------------------------------------#
+# Processing the data with R
+#------------------------------------------------------------------------------#
 # lets clean up the subject title a bit using the stringr package from tidyverse
 # first, lets get rid of the "UNVERIFIED: " section for some of these subjects
+
+# see help for stringr::str_replace
+?stringr::str_replace()
+
+# use str_replace to clean up "UNVERIFIED"
 blast2 <- blast %>%
   mutate(sub = str_replace(string = `subject title`, pattern = "UNVERIFIED: ", replacement = ""))
+# view it to confirm the "UNVERIFIED: " patterns are gone. 
+view(blast2)
 
-# now lets just use the first two words of the sub variable to pull the taxa name of the subject
+#------------------------------------------------------------------------------#
+# now lets extract the first two words of the new sub variable get the species of the subject.
+# here we'll use the stringr::word function
+
+# see help for stringr::word
+?stringr::word()
+
+# Extract the species name of the subject and assign to tax variable
 blast3 <- blast2 %>%
   mutate(tax = word(string = sub, 1, 2))
-  
-# now lets fix all the C. for Caenorhabditis
+
+# view it and see how we did? Hmm, can you see all the C. elegans, we want the full genus
+view(blast3)
+
+#------------------------------------------------------------------------------#
+# now lets replace all the "C. " patterns with "Caenorhabditis" using str_replace again
 blast4 <- blast3 %>%
   mutate(tax_fixed = str_replace(string = tax, pattern = "C\\.", replacement = "Caenorhabditis"))
 
+# view it - how'd we do?
+view(blast4)
+
+#------------------------------------------------------------------------------#
 # ok, good. Now we need to fix the `query_id`, I just want the sample name "S-XXXXX", not all the extra stuff
-# also, I don't want the ____control samples
+# also, I don't want the ____control samples.
+
+# Look at the help for str_extract, which is the only new function here.
+?stringr::str_extract()
+
+# use it
 blast5 <- blast4 %>%
   filter(!str_detect(string = `query id`, pattern = "control")) %>% # filter out queries with "control" in name
   mutate(query_fixed = str_extract(string = `query id`, pattern = "[^_]*"), .before = 1) # match everything before the first _
 
-# lastly, get just the best hit using distinct, since the top hit is on top this works without arrange
+# view it
+view(blast5)
+
+#------------------------------------------------------------------------------#
+# lastly, get just the best hit using distinct, since the best scoring hit is on top, this works without arrange.
 blast_proc <- blast5 %>%
   group_by(query_fixed) %>%
   distinct(query_fixed, .keep_all = T) %>%
@@ -36,6 +76,8 @@ blast_proc <- blast5 %>%
          perc_ident = `% identity`,
          bit_score = `bit score`,
          tax_id = tax_fixed)
+# view it
+view(blast_proc)
 
 #------------------------------------------------------------------------------#
 # lets plot the results
@@ -54,8 +96,8 @@ bar2
 
 #------------------------------------------------------------------------------#
 # Can you make a scatter plot of perc_ident vs align_len?
-# Can you color the points by bit_score
-# Does this mean anything about our confidence in the tax_id
+# Can you color the points by bit_score?
+# Does this mean anything about our confidence in the tax_id?
 # HINT - remember geom_point()?
 #------------------------------------------------------------------------------#
 ggplot(blast_proc) +
